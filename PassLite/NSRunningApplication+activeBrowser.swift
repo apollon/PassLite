73471d138183f035c05742d99aa7c962be20e646
Kostiantyn Pinchuk <apollon@users.noreply.github.com>
2023-02-10 19:43:34 +0200

    added ability to get URL for active browser tab

---
+++ PassLite/NSRunningApplication+activeBrowser.swift
@@ -0,0 +1,80 @@
+//
+//  NSRunningApplication+activeBrowser.swift
+//  PassLite
+//
+//  Created by Kostiantyn Pinchuk on 17.09.2021.
+//
+
+import AppKit
+
+extension NSRunningApplication {
+
+    var activeTabURL: NSURL?{
+        guard self.isActive, let bundleIdentifier = self.bundleIdentifier else {
+            return nil
+        }
+
+        let code: String? = {
+            switch(bundleIdentifier){
+            case "org.chromium.Chromium":
+                return "tell application \"Chromium\" to return URL of active tab of front window"
+            case "com.google.Chrome.canary":
+                return "tell application \"Google Chrome Canary\" to return URL of active tab of front window"
+            case "com.google.Chrome":
+                return "tell application \"Google Chrome\" to return URL of active tab of front window"
+            case "com.apple.Safari":
+                return "tell application \"Safari\" to return URL of front document"
+            default:
+                return nil
+            }
+        }()
+
+        var errorInfo: NSDictionary?
+        if let code = code, let script = NSAppleScript(source: code) {
+            let out: NSAppleEventDescriptor = script.executeAndReturnError(&errorInfo)
+            if let errorInfo = errorInfo{
+                print(errorInfo)
+
+            } else if let urlString = out.stringValue{
+                return NSURL(string: urlString)
+            }
+        }
+
+        return nil
+    }
+
+
+    var activeTabTitle: String?{
+        guard self.isActive, let bundleIdentifier = self.bundleIdentifier else {
+            return nil
+        }
+
+        let code: String? = {
+            switch(bundleIdentifier){
+            case "org.chromium.Chromium":
+                return "tell application \"Chromium\" to return title of active tab of front window"
+            case "com.google.Chrome.canary":
+                return "tell application \"Google Chrome Canary\" to return title of active tab of front window"
+            case "com.google.Chrome":
+                return "tell application \"Google Chrome\" to return title of active tab of front window"
+            case "com.apple.Safari":
+                return "tell application \"Safari\" to return name of front document"
+            default:
+                return nil
+            }
+        }()
+
+        var errorInfo: NSDictionary?
+        if let code = code, let script = NSAppleScript(source: code) {
+            let out: NSAppleEventDescriptor = script.executeAndReturnError(&errorInfo)
+            if let errorInfo = errorInfo{
+                print(errorInfo)
+
+            } else {
+                return out.stringValue
+            }
+        }
+
+        return nil
+    }
+}
